9d1e2c8570a7e686d7c91f8f38a14d3f40f58bc5
bwaobikeze <bwaobikeze@hotmail.com>
2024-06-26 02:26:18 -0500

    added the ability to use  the llama3 model

--- ai_chat/greet.py
+++ ai_chat/greet.py
@@ -1,9 +1,15 @@
 import gradio as gr
 import ollama as ol
 
-def greet(name, intensity):
-    return "Hello," + name + "!" * int(intensity)
+def LLMChatFunction(user_input, history):
+    # history is a list of previous messages in the chat
+    message=[{'role': 'user', 'content': user_input}]
+    try:
+        # model='llama3' is the model name
+        response = ol.chat(model='llama3', messages=message)
+        return str(response['message']['content'])
+    except Exception as e:
+        return e
     
-demo = gr.Interface(fn=greet, inputs=["text", "slider"], outputs="text")
-demo.launch()
+demo = gr.ChatInterface(LLMChatFunction).launch()
 
