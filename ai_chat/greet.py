e2782885a9c9c92da6ad1c27f678ae49b55de9b0
bwaobikeze <bwaobikeze@hotmail.com>
2024-07-02 13:34:59 -0500

    established connection to the ollama container in docker

--- ai_chat/greet.py
+++ ai_chat/greet.py
@@ -1,23 +1,19 @@
 import gradio as gr
-import requests
+import ollama as ol
 
 def LLMChatFunction(user_input, history):
     # history is a list of previous messages in the chat
     message=[{'role': 'user', 'content': user_input}]
+    ollama_server_Url = 'http://ollama:11434'
+    
+    ollama_client = ol.Client(ollama_server_Url)
     try:
-        # model='llama3' is the model name
-        url= "http://ollama:11434/v1/chat"
-        headers = {'Content-Type': 'application/json'}
-        data = {
-            "model": "llama3",
-            "messages": message
-        }
-        response = requests.post(url, headers=headers, json=data)
-        return str(response.json()['messages']['content'])
+        response = ollama_client.chat(model='llama3', messages=message)
+        return str(response['message']['content'])
     except Exception as e:
         return e
     
-demo = gr.ChatInterface(LLMChatFunction).launch()
+demo = gr.ChatInterface(LLMChatFunction).launch(server_name="0.0.0.0", server_port=7860)
 
 
     
