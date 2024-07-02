68aa9ac7dfb0544f82b1f270f81bdedaab81ad95
bwaobikeze <bwaobikeze@hotmail.com>
2024-07-02 02:20:32 -0500

    changed to a http way to use  ollama

--- ai_chat/greet.py
+++ ai_chat/greet.py
@@ -1,15 +1,23 @@
 import gradio as gr
-import ollama as ol
+import requests
 
 def LLMChatFunction(user_input, history):
     # history is a list of previous messages in the chat
     message=[{'role': 'user', 'content': user_input}]
     try:
         # model='llama3' is the model name
-        response = ol.chat(model='llama3', messages=message)
-        return str(response['message']['content'])
+        url= "http://ollama:11434/v1/chat"
+        headers = {'Content-Type': 'application/json'}
+        data = {
+            "model": "llama3",
+            "messages": message
+        }
+        response = requests.post(url, headers=headers, json=data)
+        return str(response.json()['messages']['content'])
     except Exception as e:
         return e
     
-demo = gr.ChatInterface(LLMChatFunction).launch(server_name="0.0.0.0", server_port=7860)
+demo = gr.ChatInterface(LLMChatFunction).launch()
 
+
+    
