c5cce90680f84d1f2761d3b80ed4f474d3de04a4
bwaobikeze <bwaobikeze@hotmail.com>
2024-07-03 22:59:38 -0500

    modified the return statemnt to return the chat input & chatbot response

--- ai_chat/greet.py
+++ ai_chat/greet.py
@@ -2,14 +2,17 @@ import gradio as gr
 import ollama as ol
 
 
-def LLMChatFunction(user_input, history):
+def LLMChatFunction(user_input, model_name, history):
     # history is a list of previous messages in the chat
     message=[{'role': 'user', 'content': user_input}]
     ollama_server_Url = 'http://ollama:11434'
     ollama_client = ol.Client(ollama_server_Url)
     try:
-        response = ollama_client.chat(model= 'llama3', messages=message)
-        return str(response['message']['content'])
+        response = ollama_client.chat(model= model_name, messages=message)
+        bot_response = str(response['message']['content'])
+        history.append((user_input, bot_response))
+        return "", history
+        
     except Exception as e:
         return e
 def ListOfmodels():
@@ -38,11 +41,13 @@ with gr.Blocks() as demo:
 
     available_models = ListOfmodels()
     model_dropdown = gr.Dropdown(available_models, label="Select Model")
-    
+    print(model_dropdown)
+
     PullOllamaModel(model_dropdown)
 
     chat_history = gr.State([])
+    
 
-    send_Button.click(LLMChatFunction, inputs=[user_input,chat_history], outputs=[chatbot])
+    send_Button.click(LLMChatFunction, inputs=[user_input,model_dropdown,chat_history], outputs=[chatbot])
 
 demo.launch(server_name="0.0.0.0", server_port=7860)
