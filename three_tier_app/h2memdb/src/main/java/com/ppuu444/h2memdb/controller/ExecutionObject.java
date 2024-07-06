ad437331b2263baf2b5857439801391a3316e144
bwaobikeze <bwaobikeze@hotmail.com>
2024-07-06 14:42:53 -0500

    Remove personal_llm and three_tier_app

--- three_tier_app/h2memdb/src/main/java/com/ppuu444/h2memdb/controller/ExecutionObject.java
+++
@@ -1,6 +0,0 @@
-package com.ppuu444.h2memdb.controller;
-
-public class ExecutionObject {
-    public String query;
-    public String result;
-}
--- three_tier_app/h2memdb/src/main/java/com/ppuu444/h2memdb/controller/RequestObject.java
+++
@@ -1,5 +0,0 @@
-package com.ppuu444.h2memdb.controller;
-
-public class RequestObject {
-    public String query;
-}
